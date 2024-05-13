0248cdf1eab44f2bd3e4f89c33caac0dbdcc46b7
Bruno Meireles <brunomeireles23@gmail.com>
2024-05-13 14:47:36 -0300

    added: commit inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Screen Sound
+
+string curso = "Boas vindas ao Screen Sound";
+string nome = "Bruno";
+string sobrenome = "Meireles";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("****************************************");
+    Console.WriteLine(curso);
+    Console.WriteLine(nome);
+    Console.WriteLine(sobrenome);
+    Console.WriteLine("****************************************");
+}
+
+
+ExibirMensagemDeBoasVindas();
